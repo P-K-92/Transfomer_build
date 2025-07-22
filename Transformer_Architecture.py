32b03e9578ff012a1ae9dcb04140db8088060342
Phil B-K <pkwedza@yahoo.com>
2025-07-22 23:43:49 +0200

    create the tokens for word embedding

--- Transformer_Architecture.py
+++ Transformer_Architecture.py
@@ -7,3 +7,11 @@ from torch.utils.data import DataLoader, TensorDataset
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+token_to_id = {'what': 0,
+               'is': 1,
+               'statquest': 2,
+               'awesome': 3,
+               '<EOS>': 4
+               }
+
+id_to_token = dict(map(reversed, token_to_id.items()))
